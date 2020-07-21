fd7afbae8bc8f38817b6b279053c5244038f0438
Juan Sesan <jsesan00@gmail.com>
2020-07-21 00:22:54 -0300

    JS

---
+++ boletines/assets/scripts/main.js
@@ -0,0 +1,11 @@
+
+//DOM, arbol generado por el navegador
+/*
+    Interesante demostracion de JS
+let links = document.querySelectorAll("a");
+console.log(links);
+links.forEach(function(link){
+console.log(link);
+})                  */
+
+
